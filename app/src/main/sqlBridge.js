22cd9ccaa27b79fc9fbb7475dee33ac8d9891df7
Chris laptop <bissuetox@gmail.com>
2023-05-02 20:10:19 -0700

    sql bridge format prog

--- app/src/main/sqlBridge.js
+++ app/src/main/sqlBridge.js
@@ -1,13 +1,15 @@
+const mysqlx = require('@mysql/xdevapi')
+
 export const sqlBridge = {
-    validateUser: async (payload) => {
-        return new Promise((resolve, reject) => {
-            try {
-                console.log(`Payload: ${JSON.stringify(payload, null, 2)}`)
-                const res = {msg: "Up in the :D!"}
-                resolve(res)
-            } catch (e) {
-                reject(e)
-            }
-        })
-    }
+  validateUser: async (payload) => {
+    return new Promise((resolve, reject) => {
+      try {
+        console.log(`Payload: ${JSON.stringify(payload, null, 2)}`)
+        const res = { msg: 'Up in the :D!' }
+        resolve(res)
+      } catch (e) {
+        reject(e)
+      }
+    })
+  }
 }
