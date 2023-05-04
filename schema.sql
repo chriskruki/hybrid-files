21146dcb88349cef830ebdec23058953476cdb94
Chris PC (Windows) <bissuetox@gmail.com>
2023-05-03 23:00:53 -0700

    sql schema modifications

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE `user` (
 );
 CREATE TABLE `group` (
     group_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30),
+    `name` VARCHAR(30),
     description VARCHAR(50)
 );
 CREATE TABLE user_group (
@@ -20,26 +20,26 @@ CREATE TABLE user_group (
 );
 CREATE TABLE platform (
     platform_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    type VARCHAR(15) NOT NULL,
+    `name` VARCHAR(30) NOT NULL,
+    `type` VARCHAR(15) NOT NULL,
     `schema` VARCHAR(15) NOT NULL,
-    status VARCHAR(15) NOT NULL,
+    `status` VARCHAR(15) NOT NULL,
     auth_id VARCHAR(100),
     auth_pass VARCHAR(100),
-    date_created DATE DEFAULT (CURRENT_DATE)
+    date_created TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
 );
 CREATE TABLE job (
     job_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    type VARCHAR(15) NOT NULL,
-    status VARCHAR(15) NOT NULL,
+    `name` VARCHAR(30) NOT NULL,
+    `type` VARCHAR(15) NOT NULL,
+    `status` VARCHAR(15) NOT NULL,
     src_path VARCHAR(50),
     src_platform INTEGER,
     dest_path VARCHAR(50),
     dest_platform INTEGER,
-    date_created DATE DEFAULT (CURRENT_DATE),
-    date_started DATE,
-    date_finished DATE,
+    date_created TIMESTAMP DEFAULT (CURRENT_TIMESTAMP),
+    date_started TIMESTAMP,
+    date_finished TIMESTAMP,
     FOREIGN KEY (src_platform) REFERENCES platform(platform_id),
     FOREIGN KEY (dest_platform) REFERENCES platform(platform_id)
 );
@@ -54,12 +54,12 @@ CREATE TABLE `file` (
     file_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     job_id INTEGER NOT NULL,
     platform_id INTEGER NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    path VARCHAR(50) NOT NULL,
+    `name` VARCHAR(30) NOT NULL,
+    `path` VARCHAR(50) NOT NULL,
     size INTEGER NOT NULL,
-    date_created DATE NOT NULL,
-    date_modified DATE NOT NULL,
-    date_ingested DATE NOT NULL,
+    date_created TIMESTAMP NOT NULL,
+    date_modified TIMESTAMP NOT NULL,
+    date_ingested TIMESTAMP NOT NULL DEFAULT (CURRENT_TIMESTAMP),
     FOREIGN KEY fK_file_job (job_id) REFERENCES job(job_id),
     FOREIGN KEY fK_(platform_id) REFERENCES platform(platform_id)
 );
@@ -78,8 +78,15 @@ CREATE TABLE file_group (
     FOREIGN KEY (group_id) REFERENCES `group`(group_id)
 );
 
+-- Seed admin group and user
 INSERT INTO `group` (name, description)
 VALUES ('admin', 'All Privileges');
 
 INSERT INTO `user` (username, password)
 VALUES ('admin', 'password');
+
+INSERT INTO user_group (user_id, group_id)
+VALUES (1, 1);
+
+INSERT INTO platform (`name`, `type`, `schema`, `status`)
+VALUES ('file', 'local', 'file', 'active');
