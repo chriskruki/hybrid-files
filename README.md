447db14c313191b7c8cb77ce9659c02b7f8ff67b
Chris PC (Windows) <bissuetox@gmail.com>
2023-05-03 22:52:13 -0700

    corrected sql schema, removed sql package (replaced with mysql2)

--- README.md
+++ README.md
@@ -3,6 +3,16 @@ A hybrid file storage solution for maintaining files locally and on the cloud.
 
 ## Project Setup
 
+### Create / seed database
+
+Open MySQL Command line as root user
+
+```sql
+SOURCE project_path/schema.sql
+```
+
+Where `project_path` is the path to this project
+
 ### Install
 
 ```bash
@@ -27,3 +37,7 @@ $ npm run build:mac
 # For Linux
 $ npm run build:linux
 ```
+
+# Development Mentions
+
+- `group` and `file` turned out to be reserved keywords and provided some trouble when creating tables with those names
--- schema.sql
+++ schema.sql
@@ -1,27 +1,57 @@
-CREATE TABLE user (
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    email VARCHAR(30),
-    password VARCHAR(30) NOT NULL,
-    name VARCHAR(30),
-    oauth_id VARCHAR(30)
+DROP SCHEMA IF EXISTS hybridfiles;
+CREATE SCHEMA hybridfiles;
+USE hybridfiles;
+CREATE TABLE `user` (
+    user_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30),
+    password VARCHAR(30) NOT NULL
 );
-
-CREATE TABLE group (
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE `group` (
+    group_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
-    description VARCHAR(50),
+    description VARCHAR(50)
 );
-
 CREATE TABLE user_group (
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_group_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     user_id INTEGER NOT NULL,
     group_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (group_id) REFERENCES group(id),
+    FOREIGN KEY (user_id) REFERENCES `user`(user_id),
+    FOREIGN KEY (group_id) REFERENCES `group`(group_id)
 );
-
-CREATE TABLE file (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE platform (
+    platform_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    type VARCHAR(15) NOT NULL,
+    `schema` VARCHAR(15) NOT NULL,
+    status VARCHAR(15) NOT NULL,
+    auth_id VARCHAR(100),
+    auth_pass VARCHAR(100),
+    date_created DATE DEFAULT (CURRENT_DATE)
+);
+CREATE TABLE job (
+    job_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    type VARCHAR(15) NOT NULL,
+    status VARCHAR(15) NOT NULL,
+    src_path VARCHAR(50),
+    src_platform INTEGER,
+    dest_path VARCHAR(50),
+    dest_platform INTEGER,
+    date_created DATE DEFAULT (CURRENT_DATE),
+    date_started DATE,
+    date_finished DATE,
+    FOREIGN KEY (src_platform) REFERENCES platform(platform_id),
+    FOREIGN KEY (dest_platform) REFERENCES platform(platform_id)
+);
+CREATE TABLE job_group (
+    job_group_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    job_id INTEGER NOT NULL,
+    group_id INTEGER NOT NULL,
+    FOREIGN KEY (job_id) REFERENCES job(job_id),
+    FOREIGN KEY (group_id) REFERENCES `group`(group_id)
+);
+CREATE TABLE `file` (
+    file_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     job_id INTEGER NOT NULL,
     platform_id INTEGER NOT NULL,
     name VARCHAR(30) NOT NULL,
@@ -30,58 +60,26 @@ CREATE TABLE file (
     date_created DATE NOT NULL,
     date_modified DATE NOT NULL,
     date_ingested DATE NOT NULL,
-    FOREGIN KEY (job_id) REFERENCES job(id),
-    FOREGIN KEY (platform_id) REFERENCES platform(id),
+    FOREIGN KEY fK_file_job (job_id) REFERENCES job(job_id),
+    FOREIGN KEY fK_(platform_id) REFERENCES platform(platform_id)
 );
-
-CREATE TABLE file_group (
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    file_id INTEGER NOT NULL,
-    group_id INTEGER NOT NULL,
-    FOREIGN KEY (file_id) REFERENCES file(id),
-    FOREIGN KEY (group_id) REFERENCES group(id),
-);
-
-CREATE TABLE platform (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    type VARCHAR(15) NOT NULL,
-    schema VARCHAR(15) NOT NULL,
-    status VARCHAR(15) NOT NULL,
-    auth_id VARCHAR(100),
-    auth_pass VARCHAR(100),
-    date_created DATE DEFAULT (CURRENT_DATE)
-);
-
 CREATE TABLE file_platform (
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    file_platform_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     file_id INTEGER NOT NULL,
     platform_id INTEGER NOT NULL,
-    FOREIGN KEY (file_id) REFERENCES file(id),
-    FOREIGN KEY (platform_id) REFERENCES platform(id),
+    FOREIGN KEY (file_id) REFERENCES `file`(file_id),
+    FOREIGN KEY (platform_id) REFERENCES platform(platform_id)
 );
-
-CREATE TABLE job_group (
-    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    job_id INTEGER NOT NULL,
+CREATE TABLE file_group (
+    file_group_id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    file_id INTEGER NOT NULL,
     group_id INTEGER NOT NULL,
-    FOREIGN KEY (job_id) REFERENCES job(id),
-    FOREIGN KEY (group_id) REFERENCES group(id),
+    FOREIGN KEY (file_id) REFERENCES `file`(file_id),
+    FOREIGN KEY (group_id) REFERENCES `group`(group_id)
 );
 
-CREATE TABLE job (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    type VARCHAR(15) NOT NULL,
-    status VARCHAR(15) NOT NULL,
-    src_path VARCHAR(50),
-    src_platform INTEGER,
-    dest_path VARCHAR(50),
-    dest_platform INTEGER,
-    date_created DATE DEFAULT (CURRENT_DATE)
-    date_started DATE,
-    date_finished DATE,
-    FOREIGN KEY (src_platform) REFERENCES platform(id),
-    FOREIGN KEY (dest_platform) REFERENCES platform(id),
-);
+INSERT INTO `group` (name, description)
+VALUES ('admin', 'All Privileges');
 
+INSERT INTO `user` (username, password)
+VALUES ('admin', 'password');
