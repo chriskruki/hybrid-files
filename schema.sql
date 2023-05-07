02b9a6bad776cace97f86dc11c19f3422d239404
Chris PC (Windows) <bissuetox@gmail.com>
2023-05-07 15:03:33 -0700

    user group views in schema

--- schema.sql
+++ schema.sql
@@ -77,16 +77,36 @@ CREATE TABLE file_group (
     FOREIGN KEY (file_id) REFERENCES `file`(file_id),
     FOREIGN KEY (group_id) REFERENCES `group`(group_id)
 );
-
 -- Seed admin group and user
 INSERT INTO `group` (name, description)
 VALUES ('admin', 'All Privileges');
-
 INSERT INTO `user` (username, password)
 VALUES ('admin', 'password');
-
 INSERT INTO user_group (user_id, group_id)
 VALUES (1, 1);
-
 INSERT INTO platform (`name`, `type`, `schema`, `status`)
 VALUES ('file', 'local', 'file', 'active');
+
+CREATE VIEW user_group_info AS
+SELECT u.user_id,
+    u.username,
+    u.password,
+    ug.user_group_id,
+    g.group_id,
+    g.name as group_name,
+    g.description as group_description
+FROM user u
+    INNER JOIN user_group ug on u.user_id = ug.user_id
+    INNER JOIN `group` g on ug.group_id = g.group_id;
+
+CREATE VIEW group_user_info AS
+SELECT g.group_id,
+    g.name,
+    g.description,
+    ug.user_group_id,
+    u.user_id,
+    u.username as user_username,
+    u.password as user_password
+FROM `group` g
+    INNER JOIN user_group ug on g.group_id = ug.group_id
+    INNER JOIN user u on ug.user_id = u.user_id;
--- tests.sql
+++ tests.sql
@@ -3,3 +3,31 @@ INSERT INTO platform (name, type, `schema`, status)
 
 DELETE FROM platform
 WHERE name='test'
+
+UPDATE platform
+SET `name` = 'changed',
+    `type`= 'changed',
+    `schema` = 'changed',
+    `status` = 'changed'
+WHERE platform_id = 1;
+
+SELECT *
+FROM `group`;
+
+CREATE VIEW user_group_info AS;
+
+INSERT INTO user_group (user_id, group_id)
+    VALUES (1, 3);
+
+CREATE VIEW user_group_info AS
+SELECT u.user_id, u.username, u.password, ug.user_group_id, g.group_id, g.name as group_name, g.description as group_description
+FROM user u
+INNER JOIN user_group ug on u.user_id = ug.user_id
+INNER JOIN `group` g on ug.group_id = g.group_id;
+
+CREATE VIEW group_user_info AS
+SELECT g.group_id, g.name, g.description, ug.user_group_id, u.user_id, u.username as user_username, u.password as user_password
+FROM `group` g
+INNER JOIN user_group ug on g.group_id = ug.group_id
+INNER JOIN user u on ug.user_id = u.user_id;
+
